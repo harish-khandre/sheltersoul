89ce8cc912ea219bcb1ed685f3609b75d11cf7f2
harish-khandre <harishkhandre2901@gmail.com>
2024-02-08 19:04:51 +0530

    scroll to view

--- components/home/step.tsx
+++ components/home/step.tsx
@@ -1,7 +1,7 @@
 export const Step = () => {
   return (
     <div
-      id="#step"
+      id="step"
       className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
     >
       <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
