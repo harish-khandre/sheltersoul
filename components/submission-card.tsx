4dcf4db5fc08ba29301eb348d08cff03c1c30da8
harish-khandre <harishkhandre2901@gmail.com>
2024-03-07 19:26:01 +0530

    remove background beams

--- components/submission-card.tsx
+++ components/submission-card.tsx
@@ -12,7 +12,6 @@ import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import React from "react";
-import { BackgroundBeams } from "./ui/background-beams";
 import Image from "next/image";
 
 const SubmissionCard = () => {
@@ -81,77 +80,82 @@ const SubmissionCard = () => {
   };
 
   return (
-    <div>
-      <Card className="w-[350px] relative z-10 antialiased  bg-clip-text text-transparent bg-gradient-to-b from-neutral-950 to-neutral-600">
-        <CardHeader>
-          <CardTitle>Send Help</CardTitle>
-          <CardDescription>Fill the info of the person </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={onSubmit} className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                Name (optional)
-              </label>
-              <input
-                type="text"
-                placeholder="Name of the person"
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-                className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-              />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                Location
-              </label>
-              <input
-                type="text"
-                required={true}
-                placeholder="location of the person"
-                onChange={(e) => {
-                  setLocation(e.target.value);
-                }}
-                className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm  transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-              />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                Image of the person
-              </label>
-              <input
-                type="file"
-                onChange={handleFileChange}
-                required={true}
-                accept="image/*"
-                className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-              />
-            </div>
-
-            <div className="photo-container space-y-1.5">
-              {fileUrl && file && (
-                <Image
-                  src={fileUrl}
-                  className="w-60 h-auto rounded-md"
-                  alt="img"
+    <div className=" h-full w-full bg-white bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Send Help</CardTitle>
+            <CardDescription>Fill the info of the person </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form
+              onSubmit={onSubmit}
+              className="grid w-full items-center gap-4"
+            >
+              <div className="flex flex-col space-y-1.5">
+                <label className="text-sm font-medium leading-none ">
+                  Name (optional)
+                </label>
+                <input
+                  type="text"
+                  placeholder="Name of the person"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 shadow-sm"
                 />
-              )}
-            </div>
-            <div className=" space-y-1.5">
-              <button
-                type="submit"
-                disabled={!file || uploading}
-                className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 px-4 py-2"
-              >
-                {uploading ? "Uploading..." : "Submit"}
-              </button>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex justify-between"></CardFooter>
-      </Card>
-      <BackgroundBeams />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  Location
+                </label>
+                <input
+                  type="text"
+                  required={true}
+                  placeholder="location of the person"
+                  onChange={(e) => {
+                    setLocation(e.target.value);
+                  }}
+                  className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm  transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 shadow-sm"
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                  Image of the person
+                </label>
+                <input
+                  type="file"
+                  onChange={handleFileChange}
+                  required={true}
+                  accept="image/*"
+                  className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+                />
+              </div>
+
+              <div className="photo-container space-y-1.5">
+                {fileUrl && file && (
+                  <Image
+                    src={fileUrl}
+                    className="w-60 h-auto rounded-md"
+                    alt="img"
+                  />
+                )}
+              </div>
+              <div className=" space-y-1.5">
+                <button
+                  type="submit"
+                  disabled={!file || uploading}
+                  className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 px-4 py-2 shadow"
+                >
+                  {uploading ? "Uploading..." : "Submit"}
+                </button>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-between"></CardFooter>
+        </Card>
+      </div>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,10 @@
 import type { Config } from "tailwindcss";
+const svgToDataUri = require("mini-svg-data-uri");
+const colors = require("tailwindcss/colors");
+const flattenColorPalette =
+  require("tailwindcss/lib/util/flattenColorPalette").default;
 
-const config = {
+const config: Config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -98,16 +102,22 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), [addVariablesForColors]],
-} satisfies Config;
+  plugins: [
+    require("tailwindcss-animate"),
+    addVariablesForColors,
+    addBackgroundPatterns,
+  ],
+};
 
-const {
-  default: flattenColorPalette,
-} = require("tailwindcss/lib/util/flattenColorPalette");
-
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
+function addVariablesForColors({
+  addBase,
+  theme,
+}: {
+  addBase: any;
+  theme: any;
+}) {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
   );
 
@@ -115,4 +125,37 @@ function addVariablesForColors({ addBase, theme }: any) {
     ":root": newVars,
   });
 }
+
+function addBackgroundPatterns({
+  matchUtilities,
+  theme,
+}: {
+  matchUtilities: any;
+  theme: any;
+}) {
+  matchUtilities(
+    {
+      "bg-grid": (value: any) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+        )}")`,
+      }),
+      "bg-grid-small": (value: any) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+        )}")`,
+      }),
+      "bg-dot": (value: any) => ({
+        backgroundImage: `url("${svgToDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
+        )}")`,
+      }),
+    },
+    {
+      values: flattenColorPalette(theme("backgroundColor")),
+      type: "color",
+    },
+  );
+}
+
 export default config;
